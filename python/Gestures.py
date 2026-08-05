eb6f4a5a3d5b8060c7de0ed79c75be2e930bc9b9
MatLuch <mateoluc36@gmail.com>
2026-08-05 13:22:40 -0400

    i added basic features for 2 hand use and changed the gestures, will add left and right hand next

--- python/Gestures.py
+++ python/Gestures.py
@@ -1,24 +1,30 @@
 def Gesture(lm):
 
     def Forward(lm):
-        if lm[8].y < lm[7].y < lm[5].y:
+        if lm[8].y < lm[6].y and lm[12].y < lm[10].y and lm[16].y < lm[14].y and lm[20].y < lm[18].y:
             return "forward"
         return "none"
 
-    def Backward(lm):
-        if lm[20].y < lm[19].y < lm[17].y:
-            return "backward"
+    def Stop(lm):
+        if lm[8].y > lm[6].y and lm[12].y > lm[10].y and lm[16].y > lm[14].y and lm[20].y > lm[18].y:
+            return "Stop"
         return "none"
 
-    def Left(lm):
-        if lm[12].y < lm[11].y < lm[9].y:
-            return "left"
-        return "none"
+    # def Left(lm):
+    #     if lm[12].y < lm[11].y < lm[9].y:
+    #         return "left"
+    #     return "none"
+
+    # def Right(lm):
+    #     if lm[20].y < lm[19].y < lm[17].y:
+    #         return "Right"
+    #     return "none"
 
     gestures = [
         Forward(lm),
-        Backward(lm),
-        Left(lm)
+        Stop(lm)
+        # Left(lm),
+        # Right(lm)
     ]
 
     for gestures in gestures:
@@ -26,3 +32,4 @@ def Gesture(lm):
             return gestures
 
     return "none"
+
--- python/HandLoc.py
+++ python/HandLoc.py
@@ -8,7 +8,7 @@ mp_hands = mp.solutions.hands
 mp_draw = mp.solutions.drawing_utils
 hands = mp_hands.Hands(max_num_hands=2, min_detection_confidence=0.7)
 
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1280)
 time.sleep(2)
@@ -21,12 +21,25 @@ while cap.isOpened():
     img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = hands.process(img_rgb)
 
+    left_hand = "none"
+    right_hand = "none"
+    
     if results.multi_hand_landmarks:
-        for hanLoc in results.multi_hand_landmarks:
+        for hanLoc, handType in zip(results.multi_hand_landmarks, results.multi_handedness):
             mp_draw.draw_landmarks(img, hanLoc, mp_hands.HAND_CONNECTIONS)
             lm = hanLoc.landmark
+            hand = handType.classification[0].label
+            gesture = Gesture(lm)
 
-        print(Gesture(lm))
+
+            if hand == "Left":
+                left_hand = gesture
+
+            if hand == "Right":
+                right_hand = gesture
+
+
+    print("Left:", left_hand, "Right:", right_hand)
 
     
     cv2.imshow("Hand Tracking", img)
